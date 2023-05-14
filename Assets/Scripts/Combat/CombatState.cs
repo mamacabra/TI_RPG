e994ba8f501c79ccdb93975a5a8370bc77e207a0
Jonathan Bergson <contato@bergson.me>
2023-05-13 23:08:37 -0300

    fix: os inimigos atacam pausadamente

--- Assets/Scripts/Combat/CombatState.cs
+++ Assets/Scripts/Combat/CombatState.cs
@@ -6,34 +6,43 @@ namespace Combat
     public class CombatState : MonoBehaviour
     {
         public static CombatState Instance;
-        public CombatStateType State => state;
 
-        [SerializeField] private CombatStateType state = CombatStateType.Wait;
+        private CombatStateType _state = CombatStateType.Wait;
+        private Queue<CombatStateType> _stateChanges;
         private List<ICombatStateObserver> _observers;
 
         private void Awake()
         {
             Instance = this;
+            _stateChanges = new Queue<CombatStateType>();
             _observers = new List<ICombatStateObserver>();
+        }
+
+        private void FixedUpdate()
+        {
+            while (_stateChanges.Count != 0)
+            {
+                _state = _stateChanges.Dequeue();
+                foreach (var observer in _observers)
+                    observer.OnCombatStateChanged(_state);
+            }
         }
 
         public void Subscribe(ICombatStateObserver observer)
         {
             _observers.Add(observer);
-            if (state == CombatStateType.Wait) CheckRequiredObservers();
+            if (_state == CombatStateType.Wait) CheckRequiredObservers();
         }
 
         public void SetState(CombatStateType newState)
         {
-            if (state is CombatStateType.Defeat or CombatStateType.Victory) return;
-
-            state = newState;
-            foreach (var observer in _observers) observer.OnCombatStateChanged(state);
+            if (_state is CombatStateType.Defeat or CombatStateType.Victory) return;
+            _stateChanges.Enqueue(newState);
         }
 
         public void NextState()
         {
-            switch (state)
+            switch (_state)
             {
                 case CombatStateType.PreparationStage:
                     SetState(CombatStateType.HeroTurn);
@@ -57,13 +66,16 @@ namespace Combat
         {
             bool hasCombatManager = false;
             bool hasCombatHudController = false;
+            bool hasEnemyIa = false;
+
             foreach (ICombatStateObserver observer in _observers)
             {
                 if (typeof(CombatManager) == observer.GetType()) hasCombatManager = true;
                 if (typeof(CombatHudController) == observer.GetType()) hasCombatHudController = true;
+                if (typeof(EnemyIA) == observer.GetType()) hasEnemyIa = true;
             }
 
-            if (hasCombatManager && hasCombatHudController) SetState(CombatStateType.PreparationStage);
+            if (hasCombatManager && hasCombatHudController && hasEnemyIa) SetState(CombatStateType.PreparationStage);
         }
     }
 }
--- Assets/Scripts/Combat/EnemyIA/EnemyIA.cs
+++ Assets/Scripts/Combat/EnemyIA/EnemyIA.cs
@@ -1,9 +1,12 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 namespace Combat
 {
     public class EnemyIA : MonoBehaviour, ICombatStateObserver
     {
+        private const float Delay = 0.4f;
+
         public void Start()
         {
             CombatState.Instance.Subscribe(this);
@@ -12,36 +15,21 @@ namespace Combat
         public void OnCombatStateChanged(CombatStateType state)
         {
             if (state is not CombatStateType.EnemyTurn) return;
-            AttackHeroes();
+            StartCoroutine(nameof(AttackHeroesCoroutine));
         }
 
-        private static void AttackHeroes()
+        private IEnumerator AttackHeroesCoroutine()
         {
             var enemies = CombatManager.Instance.EnemyParty.Members;
 
             enemies[0].UseRandomCard(GetRandomHero());
+            yield return new WaitForSeconds(Delay);
             enemies[1].UseRandomCard(GetRandomHero());
+            yield return new WaitForSeconds(Delay);
             enemies[2].UseRandomCard(GetRandomHero());
-
+            yield return new WaitForSeconds(Delay * 3);
             CombatState.Instance.NextState();
         }
-
-        // private static IEnumerator AttackHeroesCoroutine()
-        // {
-        //     if (CombatState.Instance.State is CombatStateType.EnemyTurn)
-        //     {
-        //         var enemies = CombatManager.Instance.Enemies;
-        //
-        //         CombatManager.UseRandomCard(enemies[0], GetRandomHero());
-        //         yield return new WaitForSeconds(1.0f);
-        //         CombatManager.UseRandomCard(enemies[1], GetRandomHero());
-        //         yield return new WaitForSeconds(1.0f);
-        //         CombatManager.UseRandomCard(enemies[2], GetRandomHero());
-        //         yield return new WaitForSeconds(1.0f);
-        //
-        //         // CombatState.Instance.NextState();
-        //     }
-        // }
 
         private static Member GetRandomHero()
         {
