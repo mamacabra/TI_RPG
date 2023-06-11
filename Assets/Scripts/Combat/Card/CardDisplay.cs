2b54004ccfa44159a4655ac0fdb7df74441890f8
Jonathan Bergson <contato@bergson.me>
2023-06-11 20:22:52 -0300

    feat: modificado posição das cartas

--- Assets/Scripts/Combat/Card/CardDisplay.cs
+++ Assets/Scripts/Combat/Card/CardDisplay.cs
@@ -4,14 +4,16 @@ namespace Combat
 {
     public class CardDisplay : MonoBehaviour
     {
-        private static readonly Vector3 CardMiddlePosition = new (0, -1, -2.5f);
-        private static readonly Vector3 Displacement = new (1.6f, 0, 0);
+        private static readonly Vector3 CardInitialPosition = new (0, -4, -5f);
+        private static readonly Vector3 CardMiddlePosition = new (0, 0.3f, -3f);
+        private static readonly Vector3 Displacement = new (1.7f, 0, 0);
 
         [SerializeField] private Vector3 cardPosition;
         private const float Speed = 10f;
 
         public void Setup(int position = 0)
         {
+            transform.position = CardInitialPosition;
             cardPosition = CardMiddlePosition + Displacement * position;
         }
 
