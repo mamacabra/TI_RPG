2c47c0bce6f44f84399f50a9a901cf01a5f54390
Renan Rodrigues Moura <93983290+Rimec@users.noreply.github.com>
2023-06-13 21:42:57 -0300

    feat: Added ui elements.

--- Assets/Scripts/VFX/VFXLine.cs
+++ Assets/Scripts/VFX/VFXLine.cs
@@ -1,23 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI.Extensions;
 using UnityEngine;
+using Unity.Mathematics;
 
-[RequireComponent(typeof(LineRenderer))]
 public class VFXLine : MonoBehaviour
 {
     [SerializeField] private Transform initialPoint;
-    private Vector3 curvePoint;
+    [SerializeField] private Vector2 initialPointOffset = new Vector2(0.0f, -2.0f);
+    private Vector2 curvePoint;
     [SerializeField][Range(-5.0f, 5.0f)] private float curve;
     [SerializeField] private Transform targetPoint;
-    [SerializeField] private Vector3 targetPointOffset;
-    private LineRenderer lineRenderer;
+    [SerializeField] private Vector2 targetPointOffset = new Vector2(0.0f, -2.0f);
+    private UILineRenderer lineRenderer;
     [SerializeField][Range(2.0f, 100.0f)] private float vertexCount = 12.0f;
     [SerializeField][Tooltip("To set the amount, change the vertex count value. ps: it might look weird if the line is too long")] private bool changeAmountOfArrows = false;
     [SerializeField] private Color color = Color.white;
+    [SerializeField] private bool mousePos = false;
+
+    Plane plane = new Plane(Vector3.back, 0);
 
     private void Awake()
     {
-        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer = GetComponent<UILineRenderer>();
         SetColor(color);
     }
 
@@ -25,36 +30,61 @@ public class VFXLine : MonoBehaviour
     {
         if (initialPoint != null && targetPoint != null)
         {
-            curvePoint = new Vector3((initialPoint.transform.position.x + targetPoint.transform.position.x) / 2, curvePoint.y, (initialPoint.transform.position.z + targetPoint.transform.position.z) / 2);
+            curvePoint = new Vector2((initialPoint.transform.position.x + targetPoint.transform.position.x) / 2, (initialPoint.transform.position.y + targetPoint.transform.position.y) / 2);
             curvePoint.y = curve;
-            var pointList = new List<Vector3>();
+            var pointList = new List<Vector2>();
 
             for (float ratio = 0; ratio <= 1; ratio += 1 / vertexCount)
             {
-                var tangent1 = Vector3.Lerp(initialPoint.position, curvePoint, ratio);
-                var tangent2 = Vector3.Lerp(curvePoint, targetPoint.position + targetPointOffset, ratio);
-                var curve = Vector3.Lerp(tangent1, tangent2, ratio);
+                var tangent1 = Vector2.Lerp((Vector2)initialPoint.position + initialPointOffset, curvePoint, ratio);
+                var tangent2 = Vector2.Lerp(curvePoint, (Vector2)targetPoint.position + targetPointOffset, ratio);
+                var curve = Vector2.Lerp(tangent1, tangent2, ratio);
 
                 pointList.Add(curve);
             }
 
-            lineRenderer.positionCount = pointList.Count;
-            lineRenderer.SetPositions(pointList.ToArray());
+            lineRenderer.Points = new Vector2[pointList.Count];
+            lineRenderer.Points = pointList.ToArray();
 
-            if (changeAmountOfArrows)
+            // if (changeAmountOfArrows)
+            // {
+            //     lineRenderer.textureMode = LineTextureMode.RepeatPerSegment;
+            // }
+            // else
+            // {
+            //     lineRenderer.textureMode = LineTextureMode.Tile;
+            // }
+        }
+        else if (mousePos)
+        {
+            Vector3 mousePosition = Vector3.zero;
+            float distance;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (plane.Raycast(ray, out distance))
             {
-                lineRenderer.textureMode = LineTextureMode.RepeatPerSegment;
+                mousePosition = ray.GetPoint(distance);
             }
-            else
+            curvePoint = new Vector2((initialPoint.transform.position.x + mousePosition.x) / 2, (initialPoint.transform.position.y + mousePosition.y) / 2);
+            curvePoint.y = curve;
+            var pointList = new List<Vector2>();
+
+            for (float ratio = 0; ratio <= 1; ratio += 1 / vertexCount)
             {
-                lineRenderer.textureMode = LineTextureMode.Tile;
+                var tangent1 = Vector2.Lerp((Vector2)initialPoint.position + initialPointOffset, curvePoint, ratio);
+                var tangent2 = Vector2.Lerp(curvePoint, (Vector2)mousePosition + targetPointOffset, ratio);
+                var curve = Vector2.Lerp(tangent1, tangent2, ratio);
+
+                pointList.Add(curve);
             }
+
+            lineRenderer.Points = new Vector2[pointList.Count];
+            lineRenderer.Points = pointList.ToArray();
+
+            lineRenderer.material.mainTextureOffset = new Vector2(math.frac(-Time.time), 0);
         }
     }
     public void SetColor(Color _color)
     {
-        // lineRenderer.material.SetColor("_BaseColor", _color);
-        // lineRenderer.material.SetColor("_EmissionColor", _color * 2.5f);
         lineRenderer.material.SetColor("_Color", _color * 2.5f);
     }
 
