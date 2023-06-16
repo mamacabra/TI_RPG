a003df0e61dd5bf9af283d443ae7a29e7e61478d
Marielle Fidelis <80335125+Miius@users.noreply.github.com>
2023-06-15 21:49:23 -0300

    Menu e logo puc

--- Assets/Scripts/Menu/MenuController.cs
+++ Assets/Scripts/Menu/MenuController.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MenuManager : MonoBehaviour
+public class MenuController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
