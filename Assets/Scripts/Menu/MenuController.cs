e39f695782bfe72e87de22b3e95ac0dda150d3cb
Marielle Fidelis <80335125+Miius@users.noreply.github.com>
2023-06-15 22:15:45 -0300

    Menu base finalizado

--- Assets/Scripts/Menu/MenuController.cs
+++ Assets/Scripts/Menu/MenuController.cs
@@ -2,18 +2,43 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Linq;
+
+
+[Serializable]
+public struct Panel
+{
+    public string panelName;
+    public GameObject panel;
+    //public Button[] buttons;
+}
 
 public class MenuController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Paineis")] [SerializeField] List<Panel> panels = new List<Panel>();
+    [SerializeField] private string firtsScreenToShow;
+
+    private void Start()
     {
-        
+        SetScreenActive(firtsScreenToShow);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PlayGame()
     {
-        
+        SceneManager.LoadScene("SampleMap");
+    }
+    public void SetScreenActive(string p)
+    {
+        foreach (var s in panels)
+            s.panel.SetActive(false);
+
+        GameObject gPanel = panels.Find(c => c.panelName == p).panel;
+        gPanel.SetActive(true);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
