34837c8609ad045ca7225dcc6f6995ac94eba62f
Escaramucho <luanassissilva@gmail.com>
2023-10-15 17:44:48 -0300

    chore: mudança do code dos icones

--- Assets/Scripts/Map/Map_Assets/IconsMovement.cs
+++ Assets/Scripts/Map/Map_Assets/IconsMovement.cs
@@ -15,18 +15,18 @@ public class IconsMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0, 5 * Time.deltaTime, 0);
+        //transform.Rotate(0, 5 * Time.deltaTime, 0);
 
         transform.position = new Vector3(transform.position.x, transform.position.y + speed * Time.deltaTime, transform.position.z);      
         
 
         if(transform.position.y >= initialPos + 0.2f)
         {
-            speed = -0.1f;
+            speed = Random.Range(-0.05f, -0.1f);
         }
         if(transform.position.y <= initialPos - 0.2f)
         {
-            speed = 0.1f;
+            speed = Random.Range(0.05f, 0.1f);
         }
         
     }
