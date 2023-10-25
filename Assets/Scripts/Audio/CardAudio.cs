c4fdbff9724bb98c0b974aece1202cc66dd54c77
Escaramucho <luanassissilva@gmail.com>
2023-10-25 17:19:40 -0300

    feat: CardSounds

---
+++ Assets/Scripts/Audio/CardAudio.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardAudio : MonoBehaviour
+{
+    [SerializeField] string ClickEffect;
+    [SerializeField] string HoverEffect;
+    [SerializeField] string NewCardEffect;
+    void Start()
+    {
+        AudioManager.audioManager.PlaySoundEffect(NewCardEffect + Random.Range(1, 5).ToString());
+    }
+
+    private void OnMouseDown()
+    {
+        AudioManager.audioManager.PlaySoundEffect(ClickEffect);
+    }
+    private void OnMouseEnter()
+    {
+        AudioManager.audioManager.PlaySoundEffect(HoverEffect + Random.Range(1,3).ToString());
+    }
+}
