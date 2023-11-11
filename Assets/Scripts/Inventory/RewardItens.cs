488c09e79223e0c9c534051119d59607277ce1ee
Marielle Fidelis <80335125+Miius@users.noreply.github.com>
2023-11-11 14:33:34 -0300

    Update RewardItens.cs

--- Assets/Scripts/Inventory/RewardItens.cs
+++ Assets/Scripts/Inventory/RewardItens.cs
@@ -8,50 +8,56 @@ using UnityEngine.UI;
 
 public class RewardItens : MonoBehaviour
 {
-   private List<ItemScriptableObject> itensToReward =new List<ItemScriptableObject>();
+   private List<ItemScriptableObject> itensToReward = new List<ItemScriptableObject>();
 
    [SerializeField] private GameObject playButton;
    [SerializeField] private GameObject bau;
    [SerializeField] private List<Transform> itensSprite;
    [SerializeField] private List<Vector3> itensSpriteFinalPos;
 
-   private void OnEnable()
+   private void Start()
    {
-      playButton.SetActive(false);
-      if (InventoryManager.instance)
-      {
-         for (int i = 0; i < 3; i++)
-         {
-            if (InventoryManager.instance.inventoryData.TryGetRandomItemPerProbability(out var item))
-            {
-               itensToReward.Add(item);
-               InventoryManager.instance.inventoryData.AddItemToInventory(item);
-               itensSprite[i].GetComponent<Image>().sprite = item.sprite;
-            }
-         }
-      }
+      GetItens();
 
+      playButton.SetActive(false);
       bau.SetActive(true);
 
       StartCoroutine(SetItensAnim());
+
       IEnumerator SetItensAnim()
       {
          yield return new WaitForSeconds(1.10f);
          for (int i = 0; i < itensSprite.Count; i++)
          {
             Transform item = itensSprite[i];
-            
-            item.DOScale(1, 0.75f).OnStart(() =>
-            {
-               item.DOLocalMove(itensSpriteFinalPos[i],1f).SetEase(Ease.OutBack);
-            }).SetEase(Ease.OutBack);
+
+            item.DOScale(1, 0.75f)
+               .OnStart(() => { item.DOLocalMove(itensSpriteFinalPos[i], 1f).SetEase(Ease.OutBack); })
+               .SetEase(Ease.OutBack);
             yield return new WaitForSeconds(0.5f);
          }
-         
+
          yield return new WaitForSeconds(1.5f);
          playButton.SetActive(true);
       }
-     
+
    }
-   
+
+   private int count = 0;
+
+   void GetItens()
+   {
+      if (InventoryManager.instance)
+      {
+         if (InventoryManager.instance.inventoryData.TryGetRandomItemPerProbability(out var item))
+         {
+            itensToReward.Add(item);
+            InventoryManager.instance.inventoryData.AddItemToInventory(item);
+            itensSprite[count].GetComponent<Image>().sprite = item.sprite;
+            count++;
+         }
+         if(count >=3) return;
+         GetItens();
+      }
+   }
 }
