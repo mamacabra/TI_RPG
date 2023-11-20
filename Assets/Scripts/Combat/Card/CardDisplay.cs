a5e8f802d41e0fd47d72727b9eb8b86e8ced2428
Jonathan Bergson <contato@bergson.me>
2023-11-20 19:55:21 -0300

    feat: cartas estao indo para nova posição sem reiniciar a animação

--- Assets/Scripts/Combat/Card/CardDisplay.cs
+++ Assets/Scripts/Combat/Card/CardDisplay.cs
@@ -4,6 +4,8 @@ namespace Combat
 {
     public class CardDisplay : MonoBehaviour
     {
+        private bool firstDisplacement = true;
+        private bool isMovement = true;
         private static readonly Vector3 CardInitialPosition = new (0, -4, -5f);
         private static readonly Vector3 CardMiddlePosition = new (0, 0.3f, -3f);
         private static readonly Vector3 Displacement = new (1.7f, 0, 0);
@@ -13,13 +15,28 @@ namespace Combat
 
         public void Setup(int position = 0)
         {
-            transform.position = CardInitialPosition;
+            isMovement = true;
+
+            if (firstDisplacement) {
+                firstDisplacement = false;
+                transform.position = CardInitialPosition;
+            }
+            
             cardPosition = CardMiddlePosition + Displacement * position;
+        }
+
+        private void FixedUpdate()
+        {
+            if (Vector3.Distance(transform.position, cardPosition) < 0.2f)
+            {
+                isMovement = false;
+                transform.position = cardPosition;
+            }
         }
 
         private void Update()
         {
-            if (Vector3.Distance(transform.position, cardPosition) > 0.2f)
+            if (isMovement)
             {
                 Vector3 direction = cardPosition - transform.position;
                 transform.position += direction.normalized * (Speed * Time.deltaTime);
