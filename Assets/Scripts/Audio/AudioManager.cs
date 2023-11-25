f4fe9d607be370c007fa8b23ec27be5416aff93f
Escaramucho <luanassissilva@gmail.com>
2023-11-25 18:04:07 -0300

    feat: dinamic audio

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -2,13 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using FMODUnity;
+using DG.Tweening;
+
+public enum SongName { Map = 0, Combat = 1}
+
+
 
 public class AudioManager : MonoBehaviour
 {
     public bool isPlay;
     public static AudioManager audioManager;
 
-
+    private FMOD.Studio.EventInstance instance;
 
 
     public FMODUnity.EventReference musicTest;
@@ -17,40 +22,96 @@ public class AudioManager : MonoBehaviour
     public FMODUnity.EventReference PlaceItem;
     public FMODUnity.EventReference EnterShop;*/
 
+    public float songType;
 
+    public bool taMudando;
 
-
-
+    float aux = 0;
     private void Awake()
     {
-        if(audioManager != null && audioManager != this)
+        if (audioManager != null && audioManager != this)
         {
             Destroy(gameObject);
         }
         else
         {
             audioManager = this;
-            PlayBackGroundSong();
+            instance = FMODUnity.RuntimeManager.CreateInstance(musicTest);
+            instance.start();
+            SetSong((int)SongName.Map);
         }
-
-
-        
-            
-        
-        DontDestroyOnLoad(audioManager);
     }
-    
+
+    private void Update()
+    {
+        
+       /* if(aux != 0 || aux != 1)
+        {
+            if (songType == 1 && aux > 0)
+            {
+                aux -= Time.deltaTime / 2;
+                if (aux < 0)
+                    aux = -0.9f;
+            }
+
+            if (songType == 0 && aux < 1)
+            {
+                aux += Time.deltaTime / 2;
+                if (aux > 1)
+                    aux = 1.1f;
+            }
+
+            Debug.Log(aux);
+            instance.setParameterByName("MapaOuCombate", aux);
+        }    */
+        
+        
+
+    }
+
 
     public void PlaySoundEffect(string Effect)
     {
         RuntimeManager.PlayOneShot("event:/"+Effect);
     }
 
-    public void PlayBackGroundSong()
+    public void SetSong(int s)
     {
-        
-         RuntimeManager.PlayOneShot(musicTest);
-        
+        StartCoroutine(Enume());
+
+        IEnumerator Enume()
+        {
+            float i = 0;
+            
+
+
+            float rate = 0.5f;
+
+            if((int)s == 0)
+            {
+                i = 1;
+                while (i > 0)
+                {
+                    i -= Time.deltaTime * rate;
+                    instance.setParameterByName("MapaOuCombate", i);
+                    yield return null;
+                }
+            }
+            else
+            {
+                while (i < 1)
+                {
+                    i += Time.deltaTime * rate;
+                    instance.setParameterByName("MapaOuCombate", i);
+                    yield return null;
+                }
+                
+            }
+                
+        }
+
     }
+
+
 
 }
