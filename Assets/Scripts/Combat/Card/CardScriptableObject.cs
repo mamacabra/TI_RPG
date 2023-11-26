6d7f5301d5e0b56a8f6054ce73c8760dd03f871b
Jonathan Bergson <contato@bergson.me>
2023-11-26 11:37:52 -0300

    feat: criado cartas, items e novos prefabs de inimigos

--- Assets/Scripts/Combat/Card/CardScriptableObject.cs
+++ Assets/Scripts/Combat/Card/CardScriptableObject.cs
@@ -9,25 +9,25 @@ namespace Combat
         [Space]
         public string label;
         [TextArea] public string description;
-        
+
         [Space]
         public Sprite sprite;
 
         [Header("Action Points")]
         [Range(-3, 3)] public int cost;
-        [Range(1, 10)] public int receive;
+        [Range(0, 10)] public int receive;
 
         [Header("Stats")]
-        [Range(1, 10)] public int damage;
-        [Range(1, 10)] public int heal;
+        [Range(0, 10)] public int damage;
+        [Range(0, 10)] public int heal;
 
         [Header("Effects")]
-        [Range(1, 3)] public int drawCard;
+        [Range(0, 3)] public int drawCard;
         // [Range(1, 3)] public int drawPartyCard;
 
         [Header("Target Effects")]
         public List<CardScriptableObject> addCardOnTargetDeck;
-        [Range(1, 3)] public int dropCardOnTargetHand;
+        [Range(0, 3)] public int dropCardOnTargetHand;
 
         [Header("Status")]
         public bool statusBewitch;
--- Assets/Scripts/Combat/CombatFactory.cs
+++ Assets/Scripts/Combat/CombatFactory.cs
@@ -6,39 +6,84 @@ namespace Combat
     public class CombatFactory : MonoBehaviour
     {
         public static CombatFactory Instance;
+        private List<Member> enemies;
 
         public Member wormLv1;
+        public Member wormLv2;
+        public Member skullLv1;
+        public Member skullLv2;
+        public Member skullLv3;
+        public Member crawfishLv1;
+
+        public enum EnemyPosition
+        {
+            Left,
+            Middle,
+            Right
+        }
 
         private void Awake()
         {
             Instance = this;
+            enemies = new List<Member>();
         }
 
         public List<Member> SpawnEnemies(int depth, IslandDirection direction)
         {
-            List<Member> enemies = new List<Member>();
-
             switch (depth)
             {
                 case 1:
-                    Member a = Instantiate(wormLv1, Vector3.zero, Quaternion.identity);
-                    Member b = Instantiate(wormLv1, Vector3.zero + Vector3.right * 5, Quaternion.identity);
-                    enemies.Add(a);
-                    enemies.Add(b);
+                    SpawnEnemy(wormLv1, EnemyPosition.Left);
+                    SpawnEnemy(wormLv1, EnemyPosition.Middle);
                     break;
                 case 2:
                     if (direction == IslandDirection.Left)
                     {
-                        Debug.Log("Spawn worm lv1");
-                        Debug.Log("Spawn worm lv2");
-                        Debug.Log("Spawn worm lv2");
+                        SpawnEnemy(wormLv1, EnemyPosition.Left);
+                        SpawnEnemy(wormLv2, EnemyPosition.Middle);
+                        SpawnEnemy(wormLv2, EnemyPosition.Right);
                     }
                     else
                     {
-                        Debug.Log("Spawn worm lv1");
-                        Debug.Log("Spawn skull lv1");
-                        Debug.Log("Spawn skull lv2");
+                        SpawnEnemy(wormLv1, EnemyPosition.Left);
+                        SpawnEnemy(skullLv1, EnemyPosition.Middle);
+                        SpawnEnemy(skullLv1, EnemyPosition.Right);
                     }
+                    break;
+                case 3:
+                    if (direction == IslandDirection.Left)
+                    {
+                        SpawnEnemy(wormLv1, EnemyPosition.Left);
+                        SpawnEnemy(skullLv2, EnemyPosition.Middle);
+                        SpawnEnemy(wormLv2, EnemyPosition.Right);
+                    }
+                    else if (direction == IslandDirection.Middle)
+                    {
+                        SpawnEnemy(skullLv3, EnemyPosition.Left);
+                        SpawnEnemy(skullLv2, EnemyPosition.Middle);
+                        SpawnEnemy(skullLv3, EnemyPosition.Right);
+                    }
+                    else
+                    {
+                        SpawnEnemy(skullLv1, EnemyPosition.Left);
+                        SpawnEnemy(skullLv2, EnemyPosition.Middle);
+                        SpawnEnemy(skullLv2, EnemyPosition.Right);
+                    }
+                    break;
+                case 4:
+                    if (direction == IslandDirection.Left)
+                    {
+                        SpawnEnemy(wormLv2, EnemyPosition.Middle);
+                        SpawnEnemy(skullLv2, EnemyPosition.Right);
+                    }
+                    else
+                    {
+                        SpawnEnemy(skullLv2, EnemyPosition.Middle);
+                        SpawnEnemy(skullLv2, EnemyPosition.Right);
+                    }
+                    break;
+                case 5:
+                    SpawnEnemy(crawfishLv1, EnemyPosition.Middle);
                     break;
                 default:
                     Debug.Log("No enemies to spawn");
@@ -46,6 +91,34 @@ namespace Combat
             }
 
             return enemies;
+        }
+
+        private void SpawnEnemy(Member enemy, EnemyPosition position)
+        {
+            Vector3 pos = new Vector3(7, 1.25f, 0);
+
+            switch (position)
+            {
+                case EnemyPosition.Left:
+                    pos.x = 3;
+                    break;
+                case EnemyPosition.Middle:
+                    pos.x = 5;
+                    break;
+                case EnemyPosition.Right:
+                    pos.x = 7;
+                    break;
+                default:
+                    Debug.LogError("Invalid enemy position");
+                    break;
+            }
+
+            if (enemy)
+            {
+                Member e = Instantiate(enemy, pos, Quaternion.identity);
+                enemies.Add(e);
+            }
+            else Debug.LogError("Enemy is null");
         }
     }
 }
