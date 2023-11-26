6508e7490e9b5d65bb1f1744cbd800d27e096c07
Jonathan Bergson <contato@bergson.me>
2023-11-26 20:46:39 -0300

    feat: adicionado timer para limpar o log

--- Assets/Scripts/Combat/CombatLog.cs
+++ Assets/Scripts/Combat/CombatLog.cs
@@ -7,9 +7,16 @@ namespace Combat
     {
         public static CombatLog Instance { get; private set; }
 
-        private int logIndex = 0;
+        private int _logIndex;
         private readonly string[] _logs = { "", "", "" };
         [SerializeField] private TMP_Text[] logTexts = new TMP_Text[3];
+
+        private float _cooldownFirst;
+        private float _cooldownSecond;
+        private float _cooldownThird;
+        private const float CooldownFirstMax = 6f;
+        private const float CooldownSecondMax = 4f;
+        private const float CooldownThirdMax = 2f;
 
         public void Awake()
         {
@@ -17,9 +24,35 @@ namespace Combat
             ClearLog();
         }
 
+        private void FixedUpdate()
+        {
+            _cooldownFirst += Time.fixedDeltaTime;
+            _cooldownSecond += Time.fixedDeltaTime;
+            _cooldownThird += Time.fixedDeltaTime;
+            if (_cooldownFirst >= CooldownFirstMax)
+            {
+                _logs[0] = "";
+                _cooldownFirst = 0;
+            }
+            if (_cooldownSecond >= CooldownSecondMax)
+            {
+                _logs[1] = "";
+                _cooldownSecond = 0;
+            }
+            if (_cooldownThird >= CooldownThirdMax)
+            {
+                _logs[2] = "";
+                _cooldownThird = 0;
+            }
+            UpdateLog();
+        }
+
         public void AddLog(string strLog)
         {
-            logIndex++;
+            _logIndex++;
+            _cooldownFirst = 0;
+            _cooldownSecond = 0;
+            _cooldownThird = 0;
             _logs[2] = _logs[1];
             _logs[1] = _logs[0];
             _logs[0] = strLog;
